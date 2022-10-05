46654703b78d5f045b93efec335987bf5ae00d89
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-10-05 20:02:58 +0300

    Javascript functionalities

--- Script.js
+++ Script.js
@@ -1,2 +1,37 @@
 const numbers = document.querySelectorAll(".numbers");
-const result = document.querySelector(".result");
+const result = document.querySelector(".result span");
+const signs = document.querySelector(".sign");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const negative = document.querySelector(".negative");
+const percent = document.querySelector(".percent");
+
+
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for(let i = 0; i < numbers.length; i++) (
+    numbers[i].addEventListener("click", (e) => {
+        let atr = e.target.getAttribute("value");
+        if(isFirstValue === false){
+            getFirstValue(atr)
+
+        }
+        if(isSecondValue == false) {
+            getSecondValue(atr);
+        }
+    })
+)
+
+function getFirstValue(el) {
+    result.innerHTML = "";
+    firstValue += el;
+    result.innerHTML = firstValue;
+    firstValue = + firstValue;
+}
+
